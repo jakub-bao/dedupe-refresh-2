65c870442313661e5e7d460297cf0a38c4ff2777
Jakub <jflaska@baosystems.com>
2020-11-05 16:43:21 -0500

    text cases

--- testData/tsDataValues/dataValues.data.ts
+++ testData/tsDataValues/dataValues.data.ts
@@ -1,77 +1,115 @@
 
 export type DedupeValue = {
     value: number;
-    attributeCategoryOption: string;
+    categoryOption_cp: string;  // Mechanism
+    dataElement_de: string; // DataElemnt
+    categoryOptionCombo_co: string; // Age Group / Sex Group
 }
 
 export type DedupeValueSet = {
-    dataElement: string;
-    attributeCategoryCombo: string;
-    categoryOptionCombo: string;
+    dataSet: string;
+    period: string;
     orgUnitId: string;
     dataValues: DedupeValue[];
 }
 
-const Nigeria1:DedupeValueSet = {
-    dataElement: "FI2s716RRZc",
-    attributeCategoryCombo: "wUpfppgjEza",
-    categoryOptionCombo: "inrjeyWelOD",
-    orgUnitId: 'BY7bSKwPjPJ',
-    dataValues: [{
-        value: 20010,
-        attributeCategoryOption: 'nwyMXmV0vPR'
-    },{
-        value: 20020,
-        attributeCategoryOption: 'EqCzVuUpqZW'
-    },{
-        value: 0,
-        attributeCategoryOption: 'xEzelmtHWPn'
-    }]
-};
-
-const Nigeria2:DedupeValueSet = {
-    dataElement: "qhGxKnmrZBd",
-    attributeCategoryCombo: "wUpfppgjEza",
-    categoryOptionCombo: "NMYN9FAPqWa",
-    orgUnitId: 'p7M264Wg1qB',
-    dataValues: [{
-        value: 30010,
-        attributeCategoryOption: 'gBvOZlOT7vE'
-    },{
-        value: 30020,
-        attributeCategoryOption: 'SnYYwQ0MNdg'
-    },{
-        value: -30010,
-        attributeCategoryOption: 'xEzelmtHWPn'
-    }]
-};
-
+// Pure dedupe, across mechanisms, unresolved
 const Rwanda1:DedupeValueSet = {
-    dataElement: "qhGxKnmrZBd",
-    attributeCategoryCombo: "wUpfppgjEza",
-    categoryOptionCombo: "xYyVHiXrvSi",
     orgUnitId: 'TAPALAZae2l',
+    dataSet: 'qzVASYuaIey',
+    period: '2020Q2',
     dataValues: [{
         value: 10010,
-        attributeCategoryOption: 'wXNP2RRZqbj'
+        categoryOption_cp: 'wXNP2RRZqbj',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "xYyVHiXrvSi",
     },{
         value: 10030,
-        attributeCategoryOption: 'xAQyTl7eVuo'
+        categoryOption_cp: 'xAQyTl7eVuo',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "xYyVHiXrvSi",
     }]
 };
 
+// Pure dedupe, across mechanisms, unresolved
 const Rwanda2:DedupeValueSet = {
-    dataElement: "qhGxKnmrZBd",
-    attributeCategoryCombo: "wUpfppgjEza",
-    categoryOptionCombo: "nEKvoyX7K7X",
     orgUnitId: 'TAPALAZae2l',
+    dataSet: 'qzVASYuaIey',
+    period: '2020Q2',
     dataValues: [{
         value: 10020,
-        attributeCategoryOption: 'wXNP2RRZqbj'
+        categoryOption_cp: 'wXNP2RRZqbj',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "nEKvoyX7K7X",
     },{
         value: 10040,
-        attributeCategoryOption: 'xAQyTl7eVuo'
+        categoryOption_cp: 'xAQyTl7eVuo',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "nEKvoyX7K7X",
     }]
 };
+
+// Pure dedupe, across mechanisms, resolved as sum
+const Nigeria1:DedupeValueSet = {
+    orgUnitId: 'BY7bSKwPjPJ',
+    dataSet: 'qzVASYuaIey',
+    period: '2020Q2',
+    dataValues: [{
+        value: 20010,
+        categoryOption_cp: 'nwyMXmV0vPR',
+        dataElement_de: 'FI2s716RRZc',
+        categoryOptionCombo_co: "inrjeyWelOD",
+    },{
+        value: 20020,
+        categoryOption_cp: 'EqCzVuUpqZW',
+        dataElement_de: 'FI2s716RRZc',
+        categoryOptionCombo_co: "inrjeyWelOD",
+    },{
+        value: 0,
+        categoryOption_cp: 'xEzelmtHWPn',
+        dataElement_de: 'FI2s716RRZc',
+        categoryOptionCombo_co: "inrjeyWelOD",
+    }]
+};
+
+// Pure dedupe, across mechanisms, resolved as max
+const Nigeria2:DedupeValueSet = {
+    orgUnitId: 'p7M264Wg1qB',
+    dataSet: 'qzVASYuaIey',
+    period: '2020Q2',
+    dataValues: [{
+        value: 30010,
+        categoryOption_cp: 'gBvOZlOT7vE',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "NMYN9FAPqWa",
+    },{
+        value: 30020,
+        categoryOption_cp: 'SnYYwQ0MNdg',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "NMYN9FAPqWa",
+    },{
+        value: -30010,
+        categoryOption_cp: 'xEzelmtHWPn',
+        dataElement_de: "qhGxKnmrZBd",
+        categoryOptionCombo_co: "NMYN9FAPqWa",
+    }]
+};
+
+// const Zambia1:DedupeValueSet = {
+//     dataSet: 'jKdHXpBfWop',
+//     orgUnitId: 'ivv9yPgimic',
+//     period: '2020Q3',
+//     dataValues: [{
+//         value: 40010,
+//         cp: 'PHHHN8hDekc',
+//         categoryOptionCombo_co: "pWaGXt8b4rt",
+//         de: 'cRCw63EQEbM',
+//     },{
+//         value: 40020,
+//         cp: 'EqCzVuUpqZW',
+//         categoryOptionCombo_co: "pWaGXt8b4rt",
+//         de: 'TiMvlchPiPH',
+//     }]
+// }
 
 export const dedupeValueSets:DedupeValueSet[] = [Nigeria1,Nigeria2,Rwanda1,Rwanda2];
--- testData/tsDataValues/insertDataValues.init.ts
+++ testData/tsDataValues/insertDataValues.init.ts
@@ -1,24 +1,21 @@
 import fetch from 'node-fetch';
 import {DedupeValue, DedupeValueSet, dedupeValueSets} from "./dataValues.data";
 
-const dataSet = 'qzVASYuaIey';
-const period = '2020Q2';
-
-function url(endpoint:string){
+function url(endpoint: string) {
     return `${process.env.DHIS_BASEURL}/api${endpoint}`;
 }
 
-dedupeValueSets.forEach((valueSet:DedupeValueSet)=>{
-    valueSet.dataValues.forEach((value:DedupeValue)=>{
-        let query = `de=${valueSet.dataElement}&co=${valueSet.categoryOptionCombo}&ds=${dataSet}&ou=${valueSet.orgUnitId}&pe=${period}&value=${value.value}&cc=${valueSet.attributeCategoryCombo}&cp=${value.attributeCategoryOption}`;
+dedupeValueSets.forEach((valueSet: DedupeValueSet) => {
+    valueSet.dataValues.forEach((value: DedupeValue) => {
+        let query = `de=${value.dataElement_de}&co=${value.categoryOptionCombo_co}&ds=${valueSet.dataSet}&ou=${valueSet.orgUnitId}&pe=${valueSet.period}&value=${value.value}&cc=wUpfppgjEza&cp=${value.categoryOption_cp}`;
         console.log(`Inserting Data Value Set > ${query}`);
-        fetch(url('/dataValues?'+query),{
+        fetch(url('/dataValues?' + query), {
             credentials: 'include',
             headers: {
                 'Authorization': 'Basic dGVzdC1kZWR1cGUtc3VwZXJBZG1pbjpDeXByZXNzMSE=',
             },
             method: 'POST',
-        }).catch(e=>{
+        }).catch(e => {
             throw e;
         })
     })

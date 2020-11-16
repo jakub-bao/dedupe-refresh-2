db14f1b007354bc378e9ab6d97461611788a6da2
Jakub <jflaska@baosystems.com>
2020-11-16 12:50:15 -0500

    before more tests

--- src/modules/results/components/resultsTable.component.tsx
+++ src/modules/results/components/resultsTable.component.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import MaterialTable, {Options} from "material-table";
+import MaterialTable, {MTableBodyRow, MTablePagination, Options} from "material-table";
 import {DedupeModel} from "../models/dedupe.model";
 import ResolutionMethodCell from "../../resolutionMethodCell/components/resolutionMethodCell.component";
 import {tableIcons} from "./resultTableIcons.component";
 import {DuplicateList} from "./duplicateList.component";
 import { colors } from "../../../values/color.values";
+import {withStyles} from "@material-ui/core";
 
 const noSort = {sorting: false};
 const padding = '5px';
 const border = '1px solid #00000021';
+const darkBorder = `1px solid rgba(0,0,0,0.3)`;
+const lightBorder = `1px solid rgba(0,0,0,0.075)`;
 const fontFamily ='"Roboto", "Helvetica", "Arial", sans-serif';
 const fontSize = '0.875rem'
 
@@ -23,16 +26,29 @@ const tableOptions:Options<DedupeModel> = {
     toolbar: false,
     headerStyle: {
         backgroundColor: 'rgb(223,223,223)',
-        borderRight: `1px solid rgba(0,0,0,0.3)`
+        borderRight: darkBorder,
+        borderBottom: darkBorder,
     }
 } as Options<DedupeModel>;
 
+export const Row = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: colors.backgroundSecondary,
+        }
+    },
+}))(MTableBodyRow);
 
+
+const components = {
+    Container:props=>(<div {...props}/>),
+    Row,
+};
 
 const columnSettings = [
-    {title: 'Data Element', field: 'info.dataElementName', cellStyle: {padding,fontFamily,fontSize}},
-    {title: 'Disaggregation', field: 'data.disAggregation', cellStyle: {padding,fontFamily,fontSize}},
-    {title: 'Org Unit', field: 'info.orgUnitName', cellStyle: {padding, borderRight: border,fontFamily,fontSize}},
+    {title: 'Data Element', field: 'info.dataElementName', cellStyle: {padding,fontFamily,fontSize, borderLeft: lightBorder}},
+    {title: 'Disaggregation', field: 'data.disAggregation', cellStyle: {padding,fontFamily,fontSize, borderLeft: lightBorder}},
+    {title: 'Org Unit', field: 'info.orgUnitName', cellStyle: {padding, borderRight: border,fontFamily,fontSize, borderLeft: lightBorder}},
     {title: 'Duplicates', render: (dedupe:DedupeModel)=><DuplicateList duplicates={dedupe.duplicates}/>, ...noSort, cellStyle: {padding:0,borderRight:border}},
     {title: 'Resolution', render: (dedupe:DedupeModel)=><ResolutionMethodCell dedupe={dedupe}/>, ...noSort, cellStyle: {padding}}
 ];
@@ -44,6 +60,6 @@ export default function ResultsTable({filteredDedupes}:{filteredDedupes: DedupeM
         options={tableOptions}
         columns={columnSettings}
         data={filteredDedupes}
-        components={{Container:props=>(<div {...props}/>)}}
+        components={components}
     />;
 }

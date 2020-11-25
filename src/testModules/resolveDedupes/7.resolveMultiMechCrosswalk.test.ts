96363aed1796126a9fb3208ba4be2c3cde668627
Jakub <jflaska@baosystems.com>
2020-11-25 12:28:00 -0500

    all tests passing

---
+++ src/testModules/resolveDedupes/7.resolveMultiMechCrosswalk.test.ts
@@ -0,0 +1,38 @@
+import {DedupeTestCase} from "../shared/models/test.models";
+import {checkCustomValue, checkStatus, searchDedupes, switchToCustom} from "../shared/sharedBasics.testService";
+import {checkRadioValue, click, type} from "../../test/domServices/click.testService";
+import {InternalStatus} from "../../modules/results/models/dedupe.model";
+import {waitForTexts} from "../../test/domServices/domUtils.testService";
+import {registerSendMock} from "../../test/apiCache/sendData/mockSendData.service";
+
+
+const EthiopiaTest:DedupeTestCase = {
+    testAs: 'test-de-superAdmin',
+    name: `Ethiopia > MER Results > 2020 Q4`,
+    filters: {
+        operatingUnit: 'Ethiopia',
+        dataType: 'MER Results',
+        period: 'Oct - Dec 2020',
+        includeResolved: true,
+        crosswalk: true
+    },
+    expectedTokens: [
+        'HTS_TST (N, TA, KeyPop/Result): HTS received results',
+        'PWID, Positive',
+        'Addis Ketema Woreda 1',
+        'HHS/HRSA',
+        'pending','80020','80010','Sum (160030)','Maximum (80020)'
+    ],
+    resolved: null,
+};
+
+test(`7 > Resolve Multi-mech Crosswalk Dedupe > Ethiopia`, async ()=>{
+    await searchDedupes(EthiopiaTest);
+    click(`resolution_1_maximum`);
+    registerSendMock('POST','/dataValues', {ok:true},(data:any)=>{
+        expect(data).toBe('de=kt5rPumWUBE&co=xYyVHiXrvSi&ou=Dl0yK0OhftZ&pe=2020Q4&value=-80010&cc=wUpfppgjEza&cp=OM58NubPbx1');
+    });
+    click(`dedupe_1_save`);
+    await waitForTexts(['Saved','resolved']);
+    checkRadioValue(`resolution_1`, 'maximum');
+});

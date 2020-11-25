e742c40d5189648e8ccdd10db3b72f84f1e8ecf1
Jakub <jflaska@baosystems.com>
2020-11-25 13:15:26 -0500

    working

--- testData/dataValues/insertDataValues.init.ts
+++ testData/dataValues/insertDataValues.init.ts
@@ -23,7 +23,7 @@ function insertDedupe(query):Promise<any>{
 dedupeValueSets.forEach((valueSet: DedupeValueSet) => {
     valueSet.dataValues.forEach(async (value: DedupeValue) => {
         let query = `de=${valueSet.dataElement_de||value.dataElement_de}&co=${valueSet.categoryOptionCombo_co||value.categoryOptionCombo_co}&ds=${valueSet.dataSet}&ou=${valueSet.orgUnitId}&pe=${valueSet.period}&value=${value.value}&cc=wUpfppgjEza&cp=${valueSet.categoryOption_cp||value.categoryOption_cp}`;
-        if (value.isResolution) await pause(2);
+        if (value.isResolution) await pause(5);
         insertDedupe(query);
     })
 });

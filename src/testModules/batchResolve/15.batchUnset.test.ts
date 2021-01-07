1eeedce40a113ea7615013f2b4ce0dcd9eae2913
Jakub <jflaska@baosystems.com>
2021-01-07 14:08:04 -0500

    working

--- src/testModules/batchResolve/15.batchUnset.test.ts
+++ src/testModules/batchResolve/15.batchUnset.test.ts
@@ -1,7 +1,9 @@
 import {clickCheckbox, searchDedupes} from "../shared/sharedBasics.testService";
 import {Nigeria1} from "../renderDedupes/3.renderDedupes.testCases";
+import {click} from "../../test/domServices/click.testService";
 
 test(`15 > Batch Unset`,async ()=>{
     await searchDedupes(Nigeria1);
     clickCheckbox(1);
+    click('batch_method_unset');
 });

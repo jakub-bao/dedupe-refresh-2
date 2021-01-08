1225b7deec03d321b762149206821696fe5c90e0
Jakub <jflaska@baosystems.com>
2021-01-08 12:45:25 -0500

    working

--- src/modules/batch/services/batchSetMethod.service.ts
+++ src/modules/batch/services/batchSetMethod.service.ts
@@ -28,7 +28,7 @@ export function batchSetMethod(dedupes:DedupeModel[], method:ResolutionMethodTyp
 export function batchUnsetMethod(dedupes:DedupeModel[]){
     getSelected(dedupes).forEach(d=>{
         let rmv:DedupeResolutionMethodValue = d.resolution.resolutionMethodValue;
-        if (!rmv.resolutionMethod) return;
+        if (!rmv||!rmv.resolutionMethod) return;
         if (!d.resolution.original_resolutionMethodValue||!d.resolution.original_resolutionMethodValue.resolutionMethod) d.status = InternalStatus.unresolved;
         else d.status = InternalStatus.readyToUnresolve;
         rmv.resolutionMethod = null;
